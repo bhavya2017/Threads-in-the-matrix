00e6dec3323f179b6b7a1bcbf94add35ca5bd5e6
bhavya2017 <113571911+bhavya2017@users.noreply.github.com>
2024-08-19 20:08:47 +0530

    Update cart.actions.js

--- src/Redux/Cart/cart.actions.js
+++ src/Redux/Cart/cart.actions.js
@@ -11,6 +11,9 @@ const CartSuccess = (payload) => {
   return { type: types.CART_SUCCESS, payload };
 };
 
+const Cartdelet = (payload) => {
+  return { type: types.DELET_CART, payload };
+};
 
 export const cartQttChange = (payload) => {
   return { type: types.CART_QTT_CHANGE, payload };
